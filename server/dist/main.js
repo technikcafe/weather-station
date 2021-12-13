a1cef9c81f7a57c20e7f8fac73949734d35b6722
Simax <simaxme@gmail.com>
2021-12-13 20:11:12 +0100

    added server

--- server/dist/main.js
+++ server/dist/main.js
@@ -1,1 +1,11 @@
-console.log("This is your main script!");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const app = express();
+app.use(express.static("./web/dist/"));
+app.get("/", (request, response) => {
+    response.sendFile("./web/dist/index.html");
+});
+app.listen(4340, () => {
+    console.log("Server started!");
+});
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,1 +1,12 @@
-console.log("This is your main script!");
+import express = require("express");
+
+const app = express();
+
+app.use(express.static("./web/dist/"));
+app.get("/", (request: express.Request, response: express.Response) => {
+    response.sendFile("./web/dist/index.html");
+});
+
+app.listen(4340, () => {
+    console.log("Server started!");
+});
